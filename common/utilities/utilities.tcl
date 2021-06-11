0d76aa1ce51d84cf597ea10e4f1f8f924116cfdd
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-06-10 23:25:56 -0500

    Modified "settemp" to be more sophisticated

--- common/utilities/utilities.tcl
+++ common/utilities/utilities.tcl
@@ -1,4 +1,4 @@
-package provide utilities 0.3
+package provide utilities 0.5
 
 namespace eval Random {
     namespace export randi
@@ -33,19 +33,17 @@ namespace eval Random {
     }
 }
 
-namespace eval tcl {
-    namespace eval mathfunc {
-        proc clamp {value min max} {
-            if {$value < $min} {
-                return $min
-            } elseif {$value > $max} {
-                return $max
-            }
-
-            return $value
-        }
+proc ::tcl::mathfunc::clamp {value min max} {
+    if {$value < $min} {
+        return $min
+    } elseif {$value > $max} {
+        return $max
     }
+
+    return $value
 }
+
+namespace eval ::constants {namespace current}
 
 # Returns an error if EXP evaluates to false.
 # Customize the error message with MSG.
@@ -66,8 +64,6 @@ proc assert {exp {msg ""}} {
 # Defines a readonly variable. Any subsequent attempts to
 # modify the variable result in an error.
 proc const {varName value} {
-    namespace eval ::constants {namespace current}
-
     if {$varName == ""} {
         error "empty var name"
     } elseif {! [regexp "^\[a-zA-z]+\[0-9_]*" $varName]} {
@@ -78,7 +74,6 @@ proc const {varName value} {
     set Var $value
 
     set "::constants::${varName}_original" $value
-
     trace add variable Var write {apply {{name1 name2 op} {
         if {$op == "write"} {
             upvar $name1 Var
@@ -98,6 +93,29 @@ proc deref {varName} {
     return $Var
 }
 
+# Implements a do-while loop.
+# Executes SCRIPT once; then, while COND evaluates to true, continues to execute SCRIPT.
+# WORD must be equal to "while".
+proc do {script word cond} {
+    if {$word ne "while"} {
+        return -code error "second parameter wrong: must be \"while\""
+    }
+
+    set condCmd [list expr $cond]
+
+    uplevel $script
+    while {[uplevel $condCmd]} {uplevel $script}
+
+    return
+}
+
+# Returns the value of the variable VAR or DEF if it doesn't exist.
+proc get {var {def ""}} {
+    upvar $var Var
+    if {[catch {set res $Var}]} {set res $def}
+    return $res
+}
+
 # Used to create a lambda function. Uses the same syntax as proc.
 proc lambda {args body} {
     set namespace [uplevel 1 namespace current]
@@ -113,6 +131,38 @@ proc lflat args {
     return $lResult
 }
 
+proc lremove {varname begin {end -1}} {
+    upvar $varname ListVar
+
+    # Convert "end" to the actual end index
+    set len [llength $ListVar]
+
+    # Calculate start index
+    set beginExpr [split $begin]
+    set begin [lindex $beginExpr 0]
+    if {$begin eq "end"} {
+        set begin [expr "$len - 1"]
+        set beginExpr [lreplace $beginExpr 0 0 $begin]
+    }
+
+    # Calculate end index
+    set endExpr [split $end]
+    set end [lindex $endExpr 0]
+    if {$end eq "end"} {
+        set end [expr "$len - 1"]
+        set endExpr [lreplace $endExpr 0 0 $end]
+    }
+    if {$end < $begin} {
+        set end $begin
+        set endExpr [lreplace $endExpr 0 0 $end]
+    }
+
+    set temp [lreplace $ListVar $beginExpr $endExpr]
+    set ListVar $temp
+
+    return
+}
+
 # Postfix increment function. Returns the old value.
 proc pincr {var {i 1}} {
     upvar $var Var
@@ -121,12 +171,48 @@ proc pincr {var {i 1}} {
     return $temp
 }
 
-# Sets a temporaru variable.
-proc settemp {sVar sValue} {
-    uplevel [list set $sVar $sValue]
-    uplevel [subst {
-        after idle {unset $sVar}
-    }]
+namespace eval ::deletequeue {
+    variable vars ""
+    variable afterid ""
 
-    return
+    namespace export settemp processQueue
+
+    proc processQueue {} {
+        variable vars
+        variable afterid
+
+        foreach v $vars {unset -nocomplain $v}
+
+        set afterid ""
+        set vars ""
+    }
+
+    proc settemp {var {value ""}} {
+        set frame [info frame -1]
+        if {[dict get $frame type] eq "proc"} {
+            return -code error "Do not call settemp within a procedure!"
+        }
+        namespace upvar ::deletequeue vars Vars
+        namespace upvar ::deletequeue afterid AfterID
+
+        # Set the value of the variable
+        uplevel [list set $var $value]
+
+        if {! [regexp {(::[a-z_]+)+} $var]} {
+            set ns [uplevel {namespace current}]
+            if {$ns eq "::"} {
+                lappend Vars "${ns}$var"
+            } else {
+                lappend Vars [join [concat $ns $var] ::]
+            }
+        }
+
+        if {$AfterID eq ""} {
+            set AfterID [after idle ::deletequeue::processQueue]
+        }
+
+        return $value
+    }
 }
+
+namespace import ::deletequeue::settemp
