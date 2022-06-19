42daa2c5ac996ceaf402d46189a84a8b7ffcf111
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-06-19 12:44:24 -0500

    tk_widgets: Improve on the exw focus command
    Use it like the traditional focus command: exw focus -displayof .window,
    for example
    
    Forgot to add exw focus to the documentation, so that's fixed now
    
    Update the associated test script

--- common/widgets/tk_focus.tcl
+++ common/widgets/tk_focus.tcl
@@ -1,5 +1,13 @@
 # tk_focus $pathname
-proc ::exWidgets::tk_focus {pathname args} {
+proc ::exWidgets::tk_focus {args} {
+    set pathname ""
+    set args [lmap e $args {
+	if {[regexp {\.[a-zA-Z0-9_]+.*} $e]} {
+	    set pathname $e
+	    continue
+	} else {set e $e}
+    }]
+
     namespace upvar [namespace current] packinfo($pathname) PackInfo
 
     set class [dict get $PackInfo class]
@@ -8,6 +16,9 @@ proc ::exWidgets::tk_focus {pathname args} {
         entry -
         tree {
             set cmd [list ::focus $pathname.$class]
+	    if {[llength $args] >= 1} {
+		set cmd [linsert $cmd 1 {*}$args]
+	    }
         }
         default {
             return -code error -errorcode [list TCL INVALID PARAM $pathname] \
--- doc/widgets-commands-std.tcl
+++ doc/widgets-commands-std.tcl
@@ -3,6 +3,26 @@
 [call exw [vset class] [opt [arg options]] [arg pathName] [opt [arg identifier]] [opt [arg options]]]
 This function creates the widget; it is described thoroughly in [sectref Description].
 
+[call exw focus [opt "-displayof [arg pathName]"]]
+Returns the path name of the focus window for the display of the main window or for the display containing
+[arg pathName] if [arg -displayof] is provided.
+In either case, an empty string is returned if the application has no focus on the given display.
+Note: [arg -displayof] is neccessary for this command to work in applications that use multiple displays.
+
+[call exw focus [arg pathName]]
+If the application currently has the input focus on [arg pathName]'s display, this command resets the focus
+to be on [arg pathName] for its display, and returns an empty string.
+If the application does not have the focus, then [arg pathName] will be remembered as the focus
+for its top level; when the application is given focus again, it will redirect that focus to [arg pathName].
+This command does nothing if [arg pathName] is an empty string.
+
+[call exw focus -force [arg pathName]]
+Sets the focus of [arg pathName]’s display to [arg pathName], even if the application does not currently have the input
+focus for the display.
+This command should be used sparingly, if at all.
+In normal usage, an application should not claim the focus for itself; instead, it should wait for the window manager to give it the focus.
+If [arg pathName] is an empty string then the command does nothing.
+
 [call exw instate [arg pathName] [arg statespec] [opt [arg script]]]
 This command tests the widget's state.
 If [arg script] is not specified, this command returns 1 if the widget state matches [arg statespec] and 0 otherwise.
