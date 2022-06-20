5416eee24a17780d17116334026b26a8b1edb8a5
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-06-19 20:40:59 -0500

    exwidgets: Refactor code and remove duplicate functions

--- common/exwidgets/__init__.py
+++ common/exwidgets/__init__.py
@@ -1,4 +1,4 @@
-"""Wrapper functions forTcl package "tk_widgets".
+"""Wrapper functions for Tcl package "tk_widgets".
 
 Example:
 root = Tk()
@@ -15,22 +15,6 @@ packageLoaded = False
 
 __all__ = ['ExWidget']
 
-def _removeUsedFromDict(d: dict):
-    result = {}
-    for k, v in d.items():
-        if v != None:
-            result[k] = v
-    return result
-
-def _joinList(l: list, sep=', ', /):
-    result = ""
-    LEN = len(l)
-    for i in range(0, LEN):
-        if i == (LEN-1):
-            sep = ''
-        result = result + str(l[i]) + sep
-    return str(result)
-
 class ExWidget:
     """Base class for extended widgets."""
 
@@ -40,11 +24,6 @@ class ExWidget:
     _substitute = Tk._substitute
     _subst_format_str = Tk._subst_format_str
     _subst_format = Tk._subst_format
-
-    #_subst_format = ('%#', '%b', '%f', '%h', '%k',
-    #         '%s', '%t', '%w', '%x', '%y',
-    #         '%A', '%E', '%K', '%N', '%W', '%T', '%X', '%Y', '%D')
-    #_subst_format_str = " ".join(_subst_format)
 
     def __init__(self, master, cnf={}, kw={}):
         # NOTE: Used for its call() method
@@ -129,6 +108,11 @@ class ExWidget:
 
         return ret
 
+    @property
+    def master(self):
+        """The widget's master."""
+        return self._master
+
     def pack(self, cnf={}, **kwargs):
         """Pack the widget in the parent widget using exw pack.
 
@@ -179,15 +163,6 @@ class ExWidget:
             res = str(res)
         return res
 
-    @property
-    def master(self):
-        """The widget's master."""
-        return self._master
-
-    def __str__(self) -> str:
-        """Returns the string path of the widget."""
-        return self._widget._w
-
     def _bind(self, what, sequence, func, add, needcleanup=1):
         """Internal function."""
         if isinstance(func, str):
@@ -206,32 +181,25 @@ class ExWidget:
         else:
             return self._tk.splitlist(self._tk.call(what))
 
-    def _register(self, func, subst=None, needcleanup=1):
-        """Return a newly created Tcl function. If this
-        function is called, the Python function FUNC will
-        be executed. An optional function SUBST can
-        be given which will be executed before FUNC."""
-        f = CallWrapper(func, subst, self).__call__
-        name = repr(id(f))
+    def _configure(self, cmd, cnf, kw):
+        """Internal function."""
+        if kw:
+            cnf = _cnfmerge((cnf, kw))
+        elif cnf:
+            cnf = _cnfmerge(cnf)
+        if cnf is None:
+            return self._getconfigure(
+                _flatten(('exw', 'subcmd', self._widget, cmd)))
+        if isinstance(cnf, str):
+            return self._getconfigure(
+                _flatten(('exw', 'subcmd', self._widget, cmd, '-'+cnf)))
+        self._tk.call(
+            _flatten(('exw', 'subcmd', self._widget, cmd)) + self._options(cnf))
 
-        try:
-            func = func.__func__
-        except AttributeError:
-            pass
-
-        try:
-            name = name + func.__name__
-        except AttributeError:
-            pass
-
-        self._tk.createcommand(name, f)
-
-        if needcleanup:
-            if self._tclCommands is None:
-                self._tclCommands = []
-            self._tclCommands.append(name)
-
-        return name
+    def _getconfigure(self, *args):
+        """Internal function."""
+        x = self._tk.splitlist(self._tk.call(*args))
+        return (x[0][1:],) + x[1:]
 
     def _options(self, cnf, kw=None):
         """Internal function."""
@@ -263,96 +231,37 @@ class ExWidget:
                 res = res + ('-'+k, v)
         return res
 
-    def instate(self, stateSpec, callback=None, *args, **kwargs):
-        """Test the widget's state.
+    def _register(self, func, subst=None, needcleanup=1):
+        """Return a newly created Tcl function. If this
+        function is called, the Python function FUNC will
+        be executed. An optional function SUBST can
+        be given which will be executed before FUNC."""
+        f = CallWrapper(func, subst, self).__call__
+        name = repr(id(f))
 
-        If CALLBACK is not specified, returns true if the widget state
-        matches STATESPEC. If CALLBACK is specified, then it will be invoked
-        if the widget state matches STATESPEC with *ARGS and **KWARGS.
-        STATESPEC is expected to be a sequence.
-        """
-        ret = self._tk.getboolean(
-            self._tk.call('exw', 'instate', self._widget, ' '.join(statespec)))
-        if ret and callback is not None:
-            return callback(*args, **kwargs)
+        try:
+            func = func.__func__
+        except AttributeError:
+            pass
 
-        return ret
+        try:
+            name = name + func.__name__
+        except AttributeError:
+            pass
 
-    def pack(self, cnf={}, **kwargs):
-        """Pack the widget in the parent widget using exw pack.
+        self._tk.createcommand(name, f)
 
-        Options include:
-        after=widget - pack it after you have packed widget
-        anchor=NSEW (or subset) - position widget according to given direction
-        before=widget - pack it before you will pack widget
-        expand=bool - expand widget if parent size grows
-        fill=NONE or X or Y or BOTH - fill widget if widget grows
-        in=master - use master to contain this widget
-        in_=master - see 'in' option description
-        ipadx=amount - add internal padding in x direction
-        ipady=amount - add internal padding in y direction
-        padx=amount - add padding in x direction
-        pady=amount - add padding in y direction
-        side=TOP or BOTTOM or LEFT or RIGHT -  where to add this widget.
-        """
-        self._tk.call(
-            ('exw', 'pack', self._widget) + self._options(cnf, kwargs)
-        )
+        if needcleanup:
+            if self._tclCommands is None:
+                self._tclCommands = []
+            self._tclCommands.append(name)
 
-    def state(self, stateSpec=None):
-        """Modify the widget's state.
-
-        Widget state is returned if STATESPEC is None, otherwise it is set
-        according to the rules outlined below. The returned value follows the same
-        rules as STATESPEC.
-
-        STATESPEC Rules
-            If the underlying widget is a ttk widget, then STATESPEC is
-            a sequence of statespec flags, but for non-ttk widgets STATESPEC is
-            either 'normal' or 'disabled'.
-        """
-        if isinstance(stateSpec, (list, tuple)):
-            stateSpec = ' '.join(stateSpec)
-        
-        return self._tk.splitlist(
-            self._tk.call('exw', 'state', self._widget, stateSpec))
-
-    def subcmd(self, cmd: str, *args):
-        """Calls one of the widget's commands.
-
-        Said command can be one of the standard commands supplied by Tk or one of the
-        extended widget commands.
-        """
-        return self._tk.call('exw', 'subcmd', self._widget, cmd, *args)
-
-    def __str__(self) -> str:
-        """Returns the string path of the widget."""
-        return self._widget._w
-
-    def _configure(self, cmd, cnf, kw):
-        """Internal function."""
-        if kw:
-            cnf = _cnfmerge((cnf, kw))
-        elif cnf:
-            cnf = _cnfmerge(cnf)
-        if cnf is None:
-            return self._getconfigure(
-                _flatten(('exw', 'subcmd', self._widget, cmd)))
-        if isinstance(cnf, str):
-            return self._getconfigure(
-                _flatten(('exw', 'subcmd', self._widget, cmd, '-'+cnf)))
-        self._tk.call(
-            _flatten(('exw', 'subcmd', self._widget, cmd)) + self._options(cnf))
-
-    def _getconfigure(self, *args):
-        """Internal function."""
-        x = self._tk.splitlist(self._tk.call(*args))
-        return (x[0][1:],) + x[1:]
+        return name
 
     def _report_exception(self):
         """Internal function."""
         self._widget._report_exception()
 
-    #def _substitute(self):
-    #    """Internal function."""
-    #    
+    def __str__(self) -> str:
+        """Returns the string path of the widget."""
+        return self._widget._w
