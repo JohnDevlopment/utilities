4fd2c3fe29855afc25a5a0b2e99aadadab533507
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-06-25 23:54:17 -0500

    exwidgets: Make custom dialog class

--- common/exwidgets/dialog.py
+++ common/exwidgets/dialog.py
@@ -4,9 +4,147 @@ Functions
     ask_string() -- ask for a string of user input
 """
 from tkinter.simpledialog import Dialog
-from tkinter import ttk, Tk, messagebox
+from tkinter import ttk, Tk, messagebox, Toplevel, _get_temp_root
+from tkinter.simpledialog import (_setup_dialog, _place_window, _destroy_temp_root,
+_get_temp_root, _destroy_temp_root)
 from .entry import ExEntry
 from .constants import *
+
+class ExDialog(Toplevel):
+    """Class to open dialogs.
+
+    This class is intended as a base class for custom dialogs.
+    """
+    def __init__(self, parent, title = None):
+        """Initialize a dialog.
+
+        Arguments:
+            parent -- a parent window (the application window)
+
+            title -- the dialog title
+        """
+        master = parent
+        if master is None:
+            master = _get_temp_root()
+
+        Toplevel.__init__(self, master)
+
+        # Remain invisible for now
+        self.withdraw()
+
+        # If the parent is not viewable, don't
+        # make the child transient, or else it
+        # would be opened withdrawn
+        if parent is not None and parent.winfo_viewable():
+            self.transient(parent)
+
+        if title:
+            self.title(title)
+
+        _setup_dialog(self)
+
+        self.parent = parent
+        self.result = None
+
+        body = ttk.Frame(self)
+        self.initial_focus = self.body(body)
+        body.pack(fill=BOTH, ipadx=5, ipady=5)
+
+        self.buttonbox()
+
+        if self.initial_focus is None:
+            self.initial_focus = self
+
+        self.protocol("WM_DELETE_WINDOW", self.cancel)
+
+        _place_window(self, parent)
+
+        self.initial_focus.focus_set()
+
+        # Wait for window to appear on screen before calling grab_set
+        self.wait_visibility()
+        self.grab_set()
+        self.wait_window(self)
+
+    def destroy(self):
+        """Destroy the window."""
+        self.initial_focus = None
+        Toplevel.destroy(self)
+        _destroy_temp_root(self.master)
+
+    #
+    # construction hooks
+
+    def body(self, master):
+        """Create dialog body.
+
+        Return widget that should have initial focus.
+        This method should be overridden, and is called
+        by the __init__() method.
+        """
+        pass
+
+    def buttonbox(self):
+        """Add standard button box.
+
+        Override if you do not want the standard buttons.
+        """
+        box = ttk.Frame(self)
+
+        w = ttk.Button(box, text='OK', width=10, command=self.ok, default=ACTIVE)
+        w.pack(side=LEFT, padx=5, pady=5)
+        w = ttk.Button(box, text="Cancel", width=10, command=self.cancel)
+        w.pack(side=LEFT, padx=5, pady=5)
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+        box.pack(fill=BOTH)
+
+    #
+    # standard button semantics
+
+    def ok(self, event=None):
+        if not self.validate():
+            self.initial_focus.focus_set() # put focus back
+            return
+
+        self.withdraw()
+        self.update_idletasks()
+
+        try:
+            self.apply()
+        finally:
+            self.cancel()
+
+    def cancel(self, event=None):
+        # put focus back to the parent window
+        if self.parent is not None:
+            self.parent.focus_set()
+        self.destroy()
+
+    #
+    # command hooks
+
+    def validate(self):
+        """Validate the data.
+
+        This method is called automatically to validate the data before the
+        dialog is destroyed. By default, it always validates OK.
+
+        Override this method to implement custom validation.
+        """
+        return True
+
+    def apply(self):
+        """Process the data.
+
+        This method is called automatically to process the data, *after*
+        the dialog is destroyed. By default, it does nothing.
+
+        Override this method to implement custom behavior.
+        """
+        pass
 
 class QueryDialog(Dialog):
     def __init__(self, parent=None, title=None, prompt=None, text=None,
@@ -55,19 +193,6 @@ class QueryDialog(Dialog):
         self.entry.pack(padx=5, fill=X)
 
         return self.entry
-
-    def buttonbox(self):
-        box = ttk.Frame(self)
-
-        w = ttk.Button(box, text='OK', width=10, command=self.ok, default=ACTIVE)
-        w.pack(side=LEFT, padx=5, pady=5)
-        w = ttk.Button(box, text="Cancel", width=10, command=self.cancel)
-        w.pack(side=LEFT, padx=5, pady=5)
-
-        self.bind("<Return>", self.ok)
-        self.bind("<Escape>", self.cancel)
-
-        box.pack()
 
     def validate(self):
         try:
